d1d79f94bc24d23cf7727306a7e5f2be7cc8551c
ny <#>
2021-05-05 14:45:06 +0800

    add logline

--- FFxivPythonTrigger/SaintCoinach.py
+++ FFxivPythonTrigger/SaintCoinach.py
@@ -1,5 +1,8 @@
 from pathlib import Path
+
+from .Logger import info
 from FFxivPythonTrigger.memory import PROCESS_FILENAME
 import pysaintcoinach
 
 realm = pysaintcoinach.ARealmReversed(Path(PROCESS_FILENAME).parent, pysaintcoinach.ex.language.Language.chinese_simplified)
+info("pysaintcoinach","realm initialized")
