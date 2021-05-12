9bec90ac76ca3dce326fdfbe1974d87261682876
ny <#>
2021-05-12 20:05:39 +0800

    bugfix

--- plugins/XivCombo/RedMage.py
+++ plugins/XivCombo/RedMage.py
@@ -1,5 +1,38 @@
 from FFxivPythonTrigger import api
 
+"""
+7503,摇荡,2
+7504,回刺,1
+7505,赤闪雷,4
+7506,短兵相接,6
+7507,赤疾风,10
+7509,散碎,15
+7510,赤火炎,26
+7511,赤飞石,30
+7512,交击斩,35
+7513,划圆斩,52
+7514,赤治疗,54
+7515,移转,40
+7516,连攻,50
+7517,飞刺,45
+7518,促进,50
+7519,六分反击,56
+7520,鼓励,58
+7521,倍增,60
+7523,赤复活,64
+7524,震荡,62
+7525,赤核爆,68
+7526,赤神圣,70
+7527,魔回刺,1
+7528,魔交击斩,35
+7529,魔连攻,50
+7530,魔划圆斩,52
+7559,沉稳咏唱,44
+7560,昏乱,8
+7561,即刻咏唱,18
+7562,醒梦,24
+"""
+
 
 def single(me):
     lv = me.level
@@ -42,6 +75,11 @@ def sword(me):
     elif combo_id == 7512 and lv >= 50:
         return 7516
     elif combo_id == 7529 and lv >= 68:
+        if lv < 70: return 7525
+        effects = me.effects.get_dict()
+        if gauge.white_mana == gauge.black_mana:
+            if 1234 in effects: return 7526
+            if 1235 in effects: return 7525
         return 7526 if white and lv >= 70 else 7525
     elif (combo_id == 7525 or combo_id == 7526) and lv >= 80:
         return 16530
