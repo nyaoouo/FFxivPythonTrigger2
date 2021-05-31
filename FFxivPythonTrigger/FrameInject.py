dc023d121e7f6b9f09b82c3ee9285b0f317f61c3
ny <#>
2021-06-01 03:06:16 +0800

    less resource

--- FFxivPythonTrigger/FrameInject.py
+++ FFxivPythonTrigger/FrameInject.py
@@ -13,10 +13,10 @@ MISSION_TIME_LIMIT = 0.05
 
 class FrameInjectHook(Hook):
     _continue_works = dict()
-    _once_works: List[Tuple[any, callable, tuple, dict]] = list()
+    _once_works: List[Tuple[callable, tuple, dict]] = list()
 
     def register_continue_call(self, call, *args, **kwargs):
-        self._continue_works[call] = (stack()[1][0], args, kwargs)
+        self._continue_works[call] = (args, kwargs)
 
     def unregister_continue_call(self, call):
         try:
@@ -25,31 +25,29 @@ class FrameInjectHook(Hook):
             pass
 
     def register_once_call(self, call, *args, **kwargs):
-        self._once_works.append((stack()[1][0], call, args, kwargs))
+        self._once_works.append((call, args, kwargs))
 
     argtypes = [c_void_p, c_void_p]
 
-    def call(self, stack, call, *args, **kwargs):
+    def call(self, call, *args, **kwargs):
         start = perf_counter()
         call(*args, **kwargs)
         use = perf_counter() - start
         if use > MISSION_TIME_LIMIT:
-            caller = getframeinfo(stack)
             _logger.warning("frame mission over time {:.2}s (limit:{:.2}s):\n"
-                            "at:\t{}:{}\n"
-                            "from:\t{}:{}".format(use, MISSION_TIME_LIMIT, getfile(call), getsourcelines(call)[1],caller.filename, caller.lineno))
+                            "at:\t{}:{}".format(use, MISSION_TIME_LIMIT, getfile(call), getsourcelines(call)[1]))
 
     def hook_function(self, *oargs):
         try:
             while self._once_works:
                 try:
-                    c_stack, call, a, k = self._once_works.pop(0)
-                    self.call(c_stack, call, *a, **k)
+                    call, a, k = self._once_works.pop(0)
+                    self.call(call, *a, **k)
                 except Exception:
                     _logger.error("error in frame call:\n" + format_exc())
             for c, v in self._continue_works.copy().items():
                 try:
-                    self.call(v[0], c, *v[1], **v[2])
+                    self.call(c, *v[1], **v[2])
                 except Exception:
                     del self._continue_works[c]
                     _logger.error("error in frame call, continue work will be removed:\n" + format_exc())
