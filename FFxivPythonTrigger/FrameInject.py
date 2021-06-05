50ab74952937e502a27edd98e7c1890f300637b9
ny <#>
2021-06-06 00:46:33 +0800

    optimize thread logic

--- FFxivPythonTrigger/FrameInject.py
+++ FFxivPythonTrigger/FrameInject.py
@@ -1,3 +1,5 @@
+from queue import Queue
+from threading import Lock
 from traceback import format_exc
 from typing import List, Tuple
 from time import perf_counter
@@ -13,19 +15,22 @@ MISSION_TIME_LIMIT = 0.05
 
 class FrameInjectHook(Hook):
     _continue_works = dict()
-    _once_works: List[Tuple[callable, tuple, dict]] = list()
+    _once_works = Queue()
+    _lock = Lock()
 
     def register_continue_call(self, call, *args, **kwargs):
-        self._continue_works[call] = (args, kwargs)
+        with self._lock:
+            self._continue_works[call] = (args, kwargs)
 
     def unregister_continue_call(self, call):
         try:
-            del self._continue_works[call]
+            with self._lock:
+                del self._continue_works[call]
         except KeyError:
             pass
 
     def register_once_call(self, call, *args, **kwargs):
-        self._once_works.append((call, args, kwargs))
+        self._once_works.put((call, args, kwargs))
 
     argtypes = [c_void_p, c_void_p]
 
@@ -39,18 +44,20 @@ class FrameInjectHook(Hook):
 
     def hook_function(self, *oargs):
         try:
-            while self._once_works:
+            while not self._once_works.empty():
                 try:
-                    call, a, k = self._once_works.pop(0)
+                    call, a, k = self._once_works.get(False)
                     self.call(call, *a, **k)
                 except Exception:
                     _logger.error("error in frame call:\n" + format_exc())
-            for c, v in self._continue_works.copy().items():
-                try:
-                    self.call(c, *v[0], **v[1])
-                except Exception:
-                    del self._continue_works[c]
-                    _logger.error("error in frame call, continue work will be removed:\n" + format_exc())
+            if self._lock.acquire(False):
+                for c, v in self._continue_works.items():
+                    try:
+                        self.call(c, *v[0], **v[1])
+                    except Exception:
+                        del self._continue_works[c]
+                        _logger.error("error in frame call, continue work will be removed:\n" + format_exc())
+                self._lock.release()
         except Exception:
             _logger.error("error in frame inject:\n" + format_exc())
         return self.original(*oargs)
