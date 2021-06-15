b66e64b5e261b47c47529f699a115b7aead743ee
ny <#>
2021-06-16 01:47:18 +0800

    bugfix

--- FFxivPythonTrigger/Utils.py
+++ FFxivPythonTrigger/Utils.py
@@ -1,8 +1,11 @@
-from typing import Callable, Tuple
+from typing import Callable, Tuple, TYPE_CHECKING
 from math import sin, cos
 
 from shapely.affinity import rotate
-from shapely.geometry import Point, Polygon, box
+
+if TYPE_CHECKING:
+    from shapely.geometry import Polygon
+
 
 def query(iterator, key: Callable[[any], bool], limit: int = None):
     count = 0
@@ -14,20 +17,19 @@ def query(iterator, key: Callable[[any], bool], limit: int = None):
                 return
 
 
-def rotated_rect(cx: float, cy: float, w: float, h: float, facing_rad: float) -> Polygon:
+def rotated_rect(cx: float, cy: float, w: float, h: float, facing_rad: float) -> 'Polygon':
+    from shapely.geometry import box
     return rotate(box(cx - w / 2, cy, cx + w / 2, cy + h), -facing_rad, origin=(cx, cy), use_radians=True)
 
 
-def circle(cx: float, cy: float, radius: float) -> Polygon:
+def circle(cx: float, cy: float, radius: float) -> 'Polygon':
+    from shapely.geometry import Point
     return Point(cx, cy).buffer(radius)
 
 
-def polar_point(cx: float, cy: float, distance: float, angle_rad: float) -> Tuple[float, float]:
-    return cx + sin(angle_rad) * distance, cy + cos(angle_rad) * distance
-
-
-def sector(cx: float, cy: float, radius: float, angle_rad: float, facing_rad: float, steps: int = 100) -> Polygon:
+def sector(cx: float, cy: float, radius: float, angle_rad: float, facing_rad: float, steps: int = 100) -> 'Polygon':
+    from shapely.geometry import Polygon
     step_angle_width = angle_rad / steps
     segment_vertices = [(cx, cy), (cx, cy + radius)]
     segment_vertices += [(cx + sin(i * step_angle_width) * radius, cy + cos(i * step_angle_width) * radius) for i in range(1, steps)]
-    return rotate(Polygon(segment_vertices), -facing_rad - angle_rad / 2, origin=(cx, cy), use_radians=True)
+    return rotate(Polygon(segment_vertices), -(facing_rad - angle_rad / 2), origin=(cx, cy), use_radians=True)
