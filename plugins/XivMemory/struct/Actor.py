adf7011d8b085d804571f4538a918d253ccb279f
ny <#>
2021-07-08 04:49:42 +0800

    fix actor hash param

--- plugins/XivMemory/struct/Actor.py
+++ plugins/XivMemory/struct/Actor.py
@@ -81,7 +81,7 @@ class Actor(OffsetStruct({
                      'is_casting'])):
 
     def __hash__(self):
-        return self.id
+        return addressof(self), self.id, self.bNpcId
 
     def __eq__(self, other):
         if type(other) == Actor:
