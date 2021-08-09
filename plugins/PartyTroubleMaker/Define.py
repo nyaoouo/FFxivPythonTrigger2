f4f42e811d7a044aeb10e2ade60f7a265b5eb2a4
ny <#>
2021-08-10 04:06:29 +0800

    add lots of things

--- plugins/PartyTroubleMaker/Define.py
+++ plugins/PartyTroubleMaker/Define.py
@@ -1,3 +1,8 @@
+from importlib import import_module
+from pathlib import Path
+
+from FFxivPythonTrigger.Logger import info
+
 party_cover_skills = {
     133,  # 医济
     131,  # 愈疗
@@ -33,10 +38,9 @@ party_cover_skills_except_source = {
     116,  # 军神
 }
 
-danger_skill = {
-    1550,  # 飞翎雨 - 极风神
-    11085,  # 飞翎雨 - 绝神兵
-}
+danger_skill = set()
+skill_alone = set()
+skill_together = dict()
 
 swift_res_jobs = {
     24,  # whm
@@ -45,3 +49,76 @@ swift_res_jobs = {
     33,  # ast
     35,  # rdm
 }
+healer = {
+    24,  # whm
+    28,  # sch
+    33,  # ast
+}
+damage_reduce = {
+    1206, 849,  # 命运之轮
+    1176,  # 武装
+    1873,  # 节制
+    299,  # 野战治疗阵
+    1951,  # 策动
+    1934,  # 行吟
+    1826,  # 防守之桑巴
+    1840,  # 石之心
+    1832,  # 伪装
+    1191,  # 铁壁
+    1834,  # 星云
+    1858,  # 原初的武猛
+    735,  # 原初的直觉
+    912,  # 受伤减轻（复仇）
+    747,  # 暗影墙
+    194,  # 铜墙铁盾
+    195,  # 坚守要塞
+    864,  # 暗黑之力
+    863,  # 原初大地
+    1931,  # 灵魂之青
+    196,  # 终极堡垒
+}
+magic_damage_reduce = {
+    317,  # 异想的幻光
+    1875,  # 炽天的幻光
+    1839,  # 光之心
+    1894,  # 暗黑布道
+    746,  # 弃明投暗
+}
+shield = {
+    727,  # 圣光幕帘
+    837,  # 黑夜领域
+    1880,  # 天星冲日（夜）
+    1898,  # 残暴弹
+    1170,  # 至黑之夜
+    297,  # 鼓舞
+    1918,  # 激励
+    1457,  # 摆脱
+    1917,  # 炽天的幕帘
+    1404,  # 神祝祷
+    168,  # 魔罩
+}
+enemy_damage_reduce = {
+    1193,  # 雪仇
+}
+enemy_physic_damage_reduce = {
+    1195,  # 牵制
+}
+enemy_magic_damage_reduce = {
+    1203,  # 昏乱
+}
+
+common_attack_name = {
+    '攻击', ''
+}
+
+for f in (Path(__file__).parent / 'datas').glob('*.py'):
+    if f.stem == '__init__': continue
+    module = import_module(f'..datas.{f.stem}', __name__)
+    if hasattr(module, 'danger_skill') and isinstance(module.danger_skill, set):
+        danger_skill |= module.danger_skill
+    if hasattr(module, 'skill_alone') and isinstance(module.skill_alone, set):
+        skill_alone |= module.skill_alone
+    if hasattr(module, 'skill_together') and isinstance(module.skill_together, dict):
+        skill_together |= module.skill_together
+damage_reduce |= magic_damage_reduce | shield
+enemy_damage_reduce |= enemy_physic_damage_reduce | enemy_magic_damage_reduce
--- plugins/PartyTroubleMaker/__init__.py
+++ plugins/PartyTroubleMaker/__init__.py
@@ -5,21 +5,26 @@ from FFxivPythonTrigger import *
 from FFxivPythonTrigger.SaintCoinach import realm
 from .Define import *
 
+command = "@ptm"
+
 action_sheet = realm.game_data.get_sheet('Action')
+status_sheet = realm.game_data.get_sheet('Status')
 job_sheet = realm.game_data.get_sheet('ClassJob')
 
 last_damage_cache = dict()
 
 
-def record_last_damage(actor_id: int, source_name: str, damage: int):
-    last_damage_cache[actor_id] = (source_name, damage, api.XivMemory.actor_table.get_actor_by_id(actor_id).currentHP, time())
+def record_last_damage(actor_id: int, source_name: str, damage: int, reduced_damage=''):
+    last_damage_cache[actor_id] = (source_name, damage, api.XivMemory.actor_table.get_actor_by_id(actor_id).currentHP, reduced_damage, time())
 
 
 def get_last_damage(actor_id: int):
     if actor_id not in last_damage_cache: return None
-    source_name, damage, current_hp, last_time = last_damage_cache[actor_id]
+    source_name, damage, current_hp, reduced_damage, last_time = last_damage_cache[actor_id]
     if last_time < time() - 30: return None
-    return f"{source_name}:{damage:,}/{current_hp:,}"
+    msg = f"{source_name}:{damage:,}/{current_hp:,}"
+    if reduced_damage: msg += " - 减伤:" + reduced_damage
+    return msg
 
 
 action_coll_down_cache = dict()
@@ -58,10 +63,16 @@ def job_name(job_id: int):
 
 @lru_cache
 def actor_name(actor_id):
+    if actor_id == 0xe0000000: return '-'
     actor = api.XivMemory.actor_table.get_actor_by_id(actor_id)
-    if actor is None: return f"unk_{actor_id}"
+    if actor is None: return f"unk_{actor_id:x}"
     # return f"{actor.Name}({actor_id:x})"
     return actor.Name
+
+
+@cache
+def status_name(status_id: int):
+    return status_sheet[status_id]['Name']
 
 
 def is_in_party():
@@ -90,68 +101,152 @@ class PartyTroubleMaker(PluginBase):
 
     def __init__(self):
         super().__init__()
+        self.lock = Lock()
         self.storage.data.setdefault('config', dict())
         self.storage.save()
+        self.last_death_count = 0
         self.register_event('network/action_effect', self.action_effect)
         self.register_event('network/actor_control/death', self.dead)
         self.register_event('network/actor_control/director_update/initial_commence', self.combat_reset)
+        api.command.register(command, self.process_command)
 
     def _onunload(self):
+        api.command.unregister(command)
         self.storage.save()
 
+    def get_mode(self, key: str):
+        return self.storage.data['config'].setdefault(key, DEFAULT_MODE)
+
     def output(self, string, msg_key: str):
-        mode = self.storage.data['config'].setdefault(msg_key, DEFAULT_MODE)
+        mode = self.get_mode(msg_key)
         if not mode: return
         cmd = '/p ' if mode == PARTY and is_in_party() else '/e '
         api.Magic.macro_command(cmd + string)
 
-    def combat_reset(self,evt):
+    def combat_reset(self, evt):
         action_coll_down_cache.clear()
         last_damage_cache.clear()
 
     def action_effect(self, event):
-        if not is_in_party(): return
-        if event.action_type == "action":
-            source_name = action_display(event.action_id, event.source_id)
-            if is_actor_in_party(event.source_id):
-                member_use_action(event.source_id, event.action_id)
-                naughty = list()
-                if event.action_id in party_cover_skills:
-                    for actor in api.XivMemory.party.main_party():
-                        if actor.id not in event.targets and actor.currentHp:
-                            naughty.append(actor_name(actor.id))
-                elif event.action_id in party_cover_skills_except_source:
-                    for actor in api.XivMemory.party.main_party():
-                        if actor.id != event.source_id and actor.id not in event.targets and actor.currentHp:
-                            naughty.append(actor_name(actor.id))
-                if naughty:
-                    self.output(f"{source_name} 未能覆盖：" + ', '.join(naughty), 'party_cover')
-            is_danger = event.action_id in danger_skill
-            for target_id, effects in event.targets.items():
-                if not is_actor_in_party(target_id): continue
-                if is_danger:
-                    self.output(f"{actor_name(target_id)} 吃了好好吃的 {source_name}", 'danger_skill')
-                for effect in effects:
-                    if 'ability' in effect.tags:
-                        record_last_damage(target_id, source_name, effect.param)
-                    elif 'instant_death' in effect.tags:
-                        record_last_damage(target_id, source_name, -1)
+        with self.lock:
+            if not is_in_party(): return
+            if event.action_type == "action":
+                source_name = action_display(event.action_id, event.source_id)
+                if is_actor_in_party(event.source_id):
+                    # 记录cd
+                    member_use_action(event.source_id, event.action_id)
+
+                    # 发送不吃团辅的坏蛋
+                    naughty = list()
+                    if event.action_id in party_cover_skills:
+                        for actor in api.XivMemory.party.main_party():
+                            if actor.id not in event.targets and actor.currentHP:
+                                naughty.append(actor_name(actor.id))
+                    elif event.action_id in party_cover_skills_except_source:
+                        for actor in api.XivMemory.party.main_party():
+                            if actor.id != event.source_id and actor.id not in event.targets and actor.currentHP:
+                                naughty.append(actor_name(actor.id))
+                    if naughty:
+                        self.output(f"{source_name} 未能覆盖：" + ', '.join(naughty), 'party_cover')
+
+                is_danger = event.action_id in danger_skill
+                take_member = []
+                for target_id, effects in event.targets.items():
+                    if not is_actor_in_party(target_id): continue
+                    take_member.append(target_id)
+
+                    # 发送吃shit队友
+                    if is_danger: self.output(f"{actor_name(target_id)} 吃了好好吃的 {source_name}", 'danger_skill')
+
+                    # 记录最后伤害
+                    for effect in effects:
+                        if 'ability' in effect.tags:
+                            r = set()
+                            t = api.XivMemory.actor_table.get_actor_by_id(target_id)
+                            if t is not None:
+                                r = {eid for eid, _ in t.effects.get_items()}.intersection(damage_reduce)
+                                s = api.XivMemory.actor_table.get_actor_by_id(event.source_id)
+                                if s is not None:r |= {eid for eid, _ in s.effects.get_items()}.intersection(enemy_damage_reduce)
+                            r = "/".join([status_name(eid) for eid in r])
+                            record_last_damage(target_id, source_name, effect.param, r)
+                            me = api.XivMemory.actor_table.get_me()
+                            if me is not None and target_id == me.id and action_name(event.action_id) not in common_attack_name:
+                                tag = '物理' if 'physics' in effect.tags else '魔法' if 'magic' in effect.tags else '?'
+                                self.output(f"{source_name} {effect.param}({tag}) 减伤：{r}", 'damage_reduce')
+                        elif 'instant_death' in effect.tags:
+                            record_last_damage(target_id, source_name, -1)
+
+                if len(take_member) > 1 and event.action_id in skill_alone:
+                    names = '，'.join([actor_name(mid) for mid in take_member])
+                    self.output(f"{names} 暖心地分享了 {source_name}", 'danger_skill')
+                elif event.action_id in skill_together and 0 < len(take_member) < skill_together[event.action_id]:
+                    names = '，'.join([actor_name(mid) for mid in take_member])
+                    self.output(f"{names} 自私地{'独吞' if len(take_member) == 1 else '瓜分'}了 {source_name}", 'danger_skill')
 
     def dead(self, event):
-        if is_actor_in_party(event.target_id):
+        with self.lock:
+            if is_actor_in_party(event.target_id):
+                # 发送死因
+                msg = f"{actor_name(event.target_id)} 被 {actor_name(event.source_id)} 击杀：{get_last_damage(event.target_id) or '未知'}"
+                self.output(msg, 'death_last_damage')
 
-            msg = f"{actor_name(event.target_id)} 被 {actor_name(event.source_id)} 击杀：{get_last_damage(event.target_id) or '未知'}"
-            self.output(msg, 'death_last_damage')
+                if self.last_death_count < time():
+                    # 发送即刻复活情况
+                    member_have_swift = list()
+                    has_healer = False
+                    has_other = False
+                    for actor in api.XivMemory.party.main_party():
+                        job = actor.job.raw_value
+                        if actor.currentHP and job in swift_res_jobs:
+                            cd = get_member_action_cd(actor.id, 7561)
+                            cd_msg = f"{cd:.1f}s" if cd else "就绪"
+                            if actor.currentMP >= 2400 and not cd:
+                                if job in healer:
+                                    has_healer = True
+                                else:
+                                    has_other = True
+                            member_have_swift.append((f"{job_name(job)}({actor.Name}):即刻{cd_msg}，蓝量{actor.currentMP}", cd))
+                    member_have_swift.sort(key=lambda x: x[1])
+                    if not member_have_swift:
+                        msg = f"没有人可以给 {actor_name(event.target_id)} 复活了，安息吧"
+                    else:
+                        msg = f"复活工具人{len(member_have_swift)}个：\n"
+                        msg += ' \n '.join([x[0] for x in member_have_swift])
+                        if has_healer:
+                            msg += f"\n请 {actor_name(event.target_id)} 等待奶妈救援"
+                        elif has_other:
+                            msg += f"\n请 {actor_name(event.target_id)} 等待救援"
+                        else:
+                            msg += f"\n躺着吧你 {actor_name(event.target_id)}"
+                    self.output(msg, 'death_count_swift')
+                    self.last_death_count = time() + 0.3
 
-            member_have_swift = list()
-            for actor in api.XivMemory.party.main_party():
-                job = actor.job.raw_value
-                if actor.currentHP and job in swift_res_jobs:
-                    cd = get_member_action_cd(actor.id, 7561)
-                    cd_msg = f"{cd:.1f}s" if cd else "ready"
-                    member_have_swift.append((f"{job_name(job)}({actor.Name}):{cd_msg}", cd))
-            if member_have_swift:
-                member_have_swift.sort(key=lambda x: x[1])
-                self.output("即刻咏唱冷却：" + (' / '.join([x[0] for x in member_have_swift])), 'death_count_swift')
-            else:
-                self.output("居然没有人可以复活，躺着吧你", 'death_count_swift')
+    def process_command(self, args):
+        try:
+            msg = self._process_command(args)
+            if msg is not None:
+                api.Magic.echo_msg(msg)
+        except Exception as e:
+            api.Magic.echo_msg(e)
+
+    def _process_command(self, args):
+        if len(args) < 2:
+            return str(self.storage.data['config'])
+        key, value = args
+        value = value.lower()
+        if value == 'disable' or value == 'd':
+            new = DISABLE
+        elif value == 'echo' or value == 'e':
+            new = ECHO
+        elif value == 'party' or value == 'p':
+            new = PARTY
+        else:
+            new = int(value)
+        if key == 'all':
+            for k in self.storage.data['config'].copy().keys():
+                self.storage.data[k] = new
+            return str(self.storage.data['config'])
+        else:
+            old = self.storage.data['config'].get(key)
+            self.storage.data['config'][key] = new
+            return f"{old} => {new}"
