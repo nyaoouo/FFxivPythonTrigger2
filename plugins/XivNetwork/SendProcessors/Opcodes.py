dc2c238a03621a72b439a24ef8aa90f3edc6feee
ny <#>
2021-08-10 19:27:51 +0800

    cn 5.5 update

--- plugins/XivNetwork/SendProcessors/Opcodes.py
+++ plugins/XivNetwork/SendProcessors/Opcodes.py
@@ -1,5 +1,18 @@
 opcodes = {
-    "2021.07.31.0000.0000": {},  # cn5.5
+    "2021.07.31.0000.0000": {
+        "Ping": 0x372,
+        # "ChatHandler": 0x0070,
+        "ClientTrigger": 0x03C1,
+        # "InventoryModifyHandler": 0x00D9,
+        # "SetSearchInfoHandler": 0x02F3,
+        "UpdatePositionHandler": 0x03B2,
+        "UpdatePositionInstance": 0x01CB,
+        # "MarketBoardPurchaseHandler": 0x0097,
+        "ActionSend": 0x2E0,
+        "EventStart": 0x2D5,
+        "EventFinish": 0x110,
+        "EventAction": 0x105,
+    },  # cn5.5
     "2021.05.28.0000.0000": {
         "Ping": 0x1bc,
         "ChatHandler": 0x0070,
